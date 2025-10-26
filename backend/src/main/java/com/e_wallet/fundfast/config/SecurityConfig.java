eb2cf7504791b0b86c98011dc376480eecb62070
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-10-27 00:23:13 +0530

    feat: add SecurityConfig class to configure security settings and permit all requests

---
+++ backend/src/main/java/com/e_wallet/fundfast/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.e_wallet.fundfast.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests((requests) -> requests
+                        // .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().permitAll())
+                .httpBasic();
+        return http.build();
+    }
+}
